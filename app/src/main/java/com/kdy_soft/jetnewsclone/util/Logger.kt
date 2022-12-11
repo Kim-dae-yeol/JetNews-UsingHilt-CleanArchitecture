bedaab600e150bd33de94cc792e8eb3cc66f67f6
gimdaeyeol <elmo7180@naver.com>
2022-12-12 06:47:28 +0900

    [Presentation Layer] 디버깅용 로거 싱글톤 클래스

---
+++ app/src/main/java/com/kdy_soft/jetnewsclone/util/Logger.kt
@@ -0,0 +1,20 @@
+package com.kdy_soft.jetnewsclone.util
+
+import android.util.Log
+
+object Logger {
+    private fun getTag(): String =
+        "${Thread.currentThread().stackTrace[4].className}.${Thread.currentThread().stackTrace[4].methodName}"
+
+    fun d(msg: String) {
+        Log.d(getTag(), msg)
+    }
+
+    fun e(msg: String) {
+        Log.e(getTag(), msg)
+    }
+
+    fun i(msg: String) {
+        Log.i(getTag(), msg)
+    }
+}
