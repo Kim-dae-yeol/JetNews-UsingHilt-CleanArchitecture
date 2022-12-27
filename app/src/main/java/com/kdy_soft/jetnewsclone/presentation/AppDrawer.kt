5db00eed058708d5357d14b7f67816d82543ecdb
gimdaeyeol <elmo7180@naver.com>
2022-12-27 20:47:43 +0900

    [Presentation]  NavDrawerItem 수정 : enum class 로 정의함으로써 forEach 를 통해 손쉽게 접근 할 수 있도록 함

--- app/src/main/java/com/kdy_soft/jetnewsclone/presentation/AppDrawer.kt
+++ app/src/main/java/com/kdy_soft/jetnewsclone/presentation/AppDrawer.kt
@@ -1,15 +1,20 @@
 package com.kdy_soft.jetnewsclone.presentation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.kdy_soft.jetnewsclone.R
+import com.kdy_soft.jetnewsclone.presentation.JetNewsNavigationRoute.HOME_ROUTE
+import com.kdy_soft.jetnewsclone.presentation.JetNewsNavigationRoute.INTERESTS_ROUTE
+import com.kdy_soft.jetnewsclone.util.Logger
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -25,31 +30,55 @@ fun AppDrawer(
             .width(120.dp)
             .systemBarsPadding()
     ) {
+        JetNewsLogo(modifier = Modifier.padding(horizontal = 28.dp, vertical = 24.dp))
         IconButton(onClick = closeDrawer) {
             Icon(imageVector = Icons.Default.Close, contentDescription = "close drawer")
         }
 
-        TextButton(onClick = {
-            navToHome()
-            closeDrawer()
-        }) {
-            val isCurrentRoute = currentRoute == JetNewsNavigationRoute.HOME_ROUTE
-            Text(
-                text = "home",
-                fontWeight = if (isCurrentRoute) FontWeight.Bold else FontWeight.Normal
-            )
-        }
+        val mainNaves = remember { JetNewsMainNavigation.values() }
+        mainNaves.forEach { item ->
+            val action = when (item.route) {
+                HOME_ROUTE -> {
+                    navToHome
+                }
+                INTERESTS_ROUTE -> {
+                    navToInterests
+                }
+                else -> {
+                    throw NoSuchElementException("Not supported route")
+                }
+            }
 
-        TextButton(onClick = {
-            navToInterests()
-            closeDrawer()
-        }) {
-            val isCurrentRoute = currentRoute == JetNewsNavigationRoute.INTERESTS_ROUTE
-            Text(
-                text = "interests",
-                fontWeight = if (isCurrentRoute) FontWeight.Bold else FontWeight.Normal
+            NavigationDrawerItem(
+                label = { Text(text = stringResource(item.labelId)) },
+                selected = item.route == currentRoute,
+                icon = {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = stringResource(item.labelId)
+                    )
+                },
+                onClick = { action();closeDrawer() },
+                modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
             )
         }
     }
 
+}
+
+@Composable
+private fun JetNewsLogo(modifier: Modifier = Modifier) {
+    Row(modifier = modifier) {
+        Icon(
+            painterResource(id = R.drawable.ic_jetnews_logo),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Icon(
+            painter = painterResource(id = R.drawable.ic_jetnews_wordmark),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.primary
+        )
+    }
 }
--- app/src/main/java/com/kdy_soft/jetnewsclone/presentation/JetNewsNavigation.kt
+++ app/src/main/java/com/kdy_soft/jetnewsclone/presentation/JetNewsNavigation.kt
@@ -1,9 +1,25 @@
 package com.kdy_soft.jetnewsclone.presentation
 
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ListAlt
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
+import com.kdy_soft.jetnewsclone.R
+import com.kdy_soft.jetnewsclone.presentation.JetNewsNavigationRoute.HOME_ROUTE
+import com.kdy_soft.jetnewsclone.presentation.JetNewsNavigationRoute.INTERESTS_ROUTE
 
+enum class JetNewsMainNavigation(
+    val route: String,
+    @StringRes val labelId: Int,
+    val icon: ImageVector
+) {
+    Home(HOME_ROUTE, R.string.home_title,Icons.Filled.Home),
+    Interest(INTERESTS_ROUTE,R.string.interest_title ,Icons.Filled.ListAlt)
+}
 
 /**
  * Main-Destinations in app, currently showing on drawer
